2ef925dd2c11eee470a61f05f7bb1d84f4e30211
pablomaruottolo <pablogabriel.maruottolo@cieep.com>
2023-09-27 12:03:19 +0200

    Initial commit

---
+++ app/src/main/java/pablo/maruottolo/ejercicio2enviardatospmdm/MotosActivity.java
@@ -0,0 +1,14 @@
+package pablo.maruottolo.ejercicio2enviardatospmdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MotosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motos);
+    }
+}
